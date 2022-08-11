9a9d84a8e2764a112dda0d12b4f93dd89dd8c0ae
Esoolgnah <nezcoreen@gmail.com>
2022-08-11 13:59:20 +0900

    :white_check_mark: test: 숫자더하기 함수 테스팅

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>Header</header>
+      <main>
+        <section>section1</section>
+        <section>section2</section>
+      </main>
+      <footer>footer</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/test/tester.test.ts
@@ -0,0 +1,14 @@
+import { addNumber } from './addNumber';
+
+test('addNumber는 두 수를 더하는 함수입니다.', () => {
+  // 조건
+  const numberA = 2;
+  const numberB = 3;
+  const result = 5;
+
+  //실행
+  const actResult = addNumber(numberA, numberB);
+
+  //평가
+  expect(actResult).toEqual(result);
+});
